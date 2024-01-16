f659a9061b114848a3bbc42b8e99624fe9aa22b3
Suponji-Ayume <503614951@qq.com>
2024-01-16 12:18:14 +0800

    增加了device的多种选择

--- DeepLearning/Models/CNN/LeNet-5/scripts/model.py
+++ DeepLearning/Models/CNN/LeNet-5/scripts/model.py
@@ -59,7 +59,12 @@ class LeNet5(nn.Module):
 # 主函数
 if __name__ == '__main__':
     # 决定使用 GPU 还是 CPU
-    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
     print('Using device:', device)
 
     # 实例化模型
